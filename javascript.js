e81e12016144bc52cade5fac2b45a6c49f74c639
Hyoso0w0 <163081966+Hyoso0w0@users.noreply.github.com>
2024-03-26 12:30:41 +0900

    third

---
+++ javascript.js
@@ -0,0 +1,29 @@
+//변수, 상수
+let maVariable = 123;
+const myConstant = "Hello, world!";
+
+console.log(myVariavle); // 123
+console.log(myConstant); // Hello, world!
+
+//데이터 타입
+let myNumber = 123;
+let myString = "Hello, world!";
+let myBoolean = true;
+let myNull = null;
+let myUndefined = undefined;
+let myObject = {name: "John", age : 30};
+let mySymbol = Symbol("mySymbol");
+
+console.log(typeof myNumber); // number
+console.log(typeof myString); // string
+console.log(typeof myBoolean); // boolean
+console.log(typeof myNull); // object
+console.log(typeof myUndefined); // undefined
+console.log(typeof myObject); // object
+console.log(typeof mySymbol); // symbol
+
+//연산자
+let x = 10;
+let y = 5;
+
+console.log(x + y); // 15
